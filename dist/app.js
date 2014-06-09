6e57e5e1dee6e1ce5490cf6aa78ce74854f528e2
netpork <netpork@gmail.com>
2014-06-09 18:00:36 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,28 @@
+var Telemob = (function($) {
+	'use strict';
+
+	function initialize() {
+		console.log($);
+
+
+
+
+	}
+
+
+
+	// start
+	// $(function(){
+		initialize();
+	// });
+
+	
+
+
+
+
+	return {
+		initialize: initialize
+	};
+
+})(angular);
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+	var gulp = require('gulp'),
+		gutil = require('gulp-util'),
+		uglify = require('gulp-uglify'),
+		concat = require('gulp-concat'),
+		connect = require('gulp-connect'),
+		bower = require('gulp-bower-src'),
+		gulpFilter = require('gulp-filter'),
+		debug = require('gulp-debug'),
+		inject = require('gulp-inject'),
+		es = require('event-stream'),
+		watch = require('gulp-watch')
+	;
+
+	var filter = gulpFilter('**/*.min.js');
+
+	gulp.task('webserver', function() {
+		connect.server({
+			livereload: true
+		});
+	});
+
+	gulp.task('boweride', function() {
+		bower()
+			.pipe(filter)
+			// .pipe(uglify())
+			// .pipe(filter.restore())
+			// .pipe(debug())
+			.pipe(gulp.dest('./lib'));
+	});
+
+	gulp.task('js', function() {
+		var vendorStream = gulp.src('./lib/**/*.min.js')
+			.pipe(concat('vendors.js'))
+			.pipe(gulp.dest('./dist'));
+
+		var appStream = gulp.src('assets/js/*.js')
+			.pipe(concat('app.js'))
+			// .pipe(uglify())
+			.pipe(gulp.dest('./dist'));
+
+		gulp.src('./index.html')
+			.pipe(inject(es.merge(appStream, vendorStream)))
+			.pipe(gulp.dest('./dist'));
+	});
+
+	gulp.task('watch', function() {
+		gulp.watch(['assets/js/*.js', './index.html'], ['js']);
+	});
+
+	gulp.task('livereload', function() {
+		gulp.src('./dist/*.js')
+			.pipe(watch())
+			.pipe(connect.reload());
+	});
+
+	// gulp.task('default', ['webserver', 'boweride']);
+	gulp.task('default', ['boweride', 'js', 'webserver', 'livereload', 'watch']);
